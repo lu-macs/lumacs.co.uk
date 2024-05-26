939c40d8839036441c56752246c01a4545bcfc13
tonyaellie <tonya@tokia.dev>
2024-05-26 23:56:38 +0000

    fix: just embed the db url

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,13 +3,13 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless';
 
 import * as schema from './schema';
 
+// ENVIRONMENT VARIABLES ARE NOT AVAILABLE in production
+
 export const db =
   import.meta.env.NODE_ENV !== 'development'
     ? drizzle(
         new Client({
-          host: import.meta.env.DATABASE_HOST,
-          username: import.meta.env.DATABASE_USERNAME,
-          password: import.meta.env.DATABASE_PASSWORD,
+          url: 'replace-with-the-database-url',
           fetch: (url, init) => {
             init && delete init['cache'];
             return fetch(url, init);
