b81da4103eef7fcb9e312bc04c872fee39b0c2e2
tonyaellie <tonya@tokia.dev>
2024-05-26 23:43:00 +0000

    fix: try fixing env vars

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,22 +3,23 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless';
 
 import * as schema from './schema';
 
-export const db = import.meta.env.PROD
-  ? drizzle(
-      new Client({
-        host: import.meta.env.DATABASE_HOST,
-        username: import.meta.env.DATABASE_USERNAME,
-        password: import.meta.env.DATABASE_PASSWORD,
-        fetch: (url, init) => {
-          init && delete init['cache'];
-          return fetch(url, init);
-        },
-      }),
-      { schema }
-    )
-  : drizzle(
-      new Client({
-        url: import.meta.env.DATABASE_URL,
-      }),
-      { schema }
-    );
+export const db =
+  import.meta.env.NODE_ENV !== 'development'
+    ? drizzle(
+        new Client({
+          host: import.meta.env.DATABASE_HOST,
+          username: import.meta.env.DATABASE_USERNAME,
+          password: import.meta.env.DATABASE_PASSWORD,
+          fetch: (url, init) => {
+            init && delete init['cache'];
+            return fetch(url, init);
+          },
+        }),
+        { schema }
+      )
+    : drizzle(
+        new Client({
+          url: import.meta.env.DATABASE_URL,
+        }),
+        { schema }
+      );
