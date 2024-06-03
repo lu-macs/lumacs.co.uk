4bc5b3698e1a0038e49e58cb1c06e5b2a7b80b74
tonya <tonya@tokia.dev>
2024-06-03 08:01:16 +0000

    feat: add cmdk menu

---
+++ src/components/CommandSearch.tsx
@@ -0,0 +1,97 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from '@/components/ui/command';
+import { useEffect, useState } from 'react';
+import { navigate } from 'astro:transitions/client';
+
+export const CommandSearch = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen}>
+      <CommandInput placeholder="Type a search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+              navigate('/');
+            }}
+          >
+            Home
+          </CommandItem>
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+              navigate(
+                'https://lancastersu.co.uk/groups/magic-and-circus-society-lumacs/join'
+              );
+            }}
+          >
+            Join Us
+          </CommandItem>
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+              navigate('/hire/');
+            }}
+          >
+            Hire Us
+          </CommandItem>
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+              navigate('/gallery/');
+            }}
+          >
+            Gallery
+          </CommandItem>
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+              navigate('/conventions/');
+            }}
+          >
+            Conventions
+          </CommandItem>
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+              navigate('/timeline/');
+            }}
+          >
+            Our History
+          </CommandItem>
+          <CommandItem
+            onSelect={() => {
+              setOpen(false);
+              navigate('/privacy/');
+            }}
+          >
+            Privacy Policy
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  );
+};
