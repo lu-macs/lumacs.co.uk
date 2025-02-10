69a76fddf6f4772979928b443e7a4e248a2384f3
tonyaellie <tonya@tokia.dev>
2025-02-10 22:43:12 +0000

    feat: add wait until (breaks locally)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface WorkerRuntime {
+  runtime: {
+    waitUntil: (promise: Promise<any>) => void;
+    env: Env;
+    cf: CFRequest['cf'];
+    caches: typeof caches;
+  };
+}
+
+declare namespace App {
+  interface Locals extends WorkerRuntime {}
+}
--- src/pages/q/[short].ts
+++ src/pages/q/[short].ts
@@ -6,10 +6,7 @@ redirects.set(
   'vote',
   'https://docs.google.com/forms/d/e/1FAIpQLSdQbtldzvZ76Bp57kv4P38s9TQy1bneilsPVwNx3i1I_40jkg/viewform'
 );
-redirects.set(
-  'insta',
-  'https://www.instagram.com/lu_macs/'
-);
+redirects.set('insta', 'https://www.instagram.com/lu_macs/');
 redirects.set(
   'join',
   'https://lancastersu.co.uk/groups/magic-and-circus-society-lumacs'
@@ -18,10 +15,7 @@ redirects.set(
   'christmas',
   'https://docs.google.com/forms/d/e/1FAIpQLScw_gNTtdFR3bW9DBAYuNtB_mGT9Uj0XZJfB_7nPEvirkB9wQ/viewform'
 );
-redirects.set(
-  'testing',
-  'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-);
+redirects.set('testing', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 redirects.set(
   'lucc',
   'https://lancastersu.co.uk/groups/magic-and-circus-society-lumacs/events/lucc-lumacs-convention-2025'
@@ -30,16 +24,14 @@ redirects.set(
   'merch',
   'https://docs.google.com/presentation/d/1ZwXCOiEtYBMuc-Rc0N7E_uTkEvyVE5Tz67KDuLHbxH4/edit?usp=sharing'
 );
-redirects.set(
-  'reject',
-  'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-);
+redirects.set('reject', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
 export const GET: APIRoute = async ({
   params,
   redirect,
   request,
   clientAddress,
+  locals: { runtime },
 }) => {
   const id = params.short?.toLowerCase();
 
@@ -47,25 +39,27 @@ export const GET: APIRoute = async ({
     return redirect(`/qnf/${id}/`, 307);
   }
 
-  await fetch('https://ingest.tokia.dev/api/event', {
-    method: 'POST',
-    headers: {
-      'User-Agent': request.headers.get('User-Agent') ?? '',
-      'X-Forwarded-For':
-        request.headers.get('X-Forwarded-For') ?? clientAddress,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      domain: 'lumacs.co.uk',
-      name: 'Redirect',
-      url: request.url,
-      referrer: request.headers.get('Referer') ?? '',
-      props: {
-        from: id,
-        to: redirects.get(id)!,
+  runtime.waitUntil(
+    fetch('https://ingest.tokia.dev/api/event', {
+      method: 'POST',
+      headers: {
+        'User-Agent': request.headers.get('User-Agent') ?? '',
+        'X-Forwarded-For':
+          request.headers.get('X-Forwarded-For') ?? clientAddress,
+        'Content-Type': 'application/json',
       },
-    }),
-  });
+      body: JSON.stringify({
+        domain: 'lumacs.co.uk',
+        name: 'Redirect',
+        url: request.url,
+        referrer: request.headers.get('Referer') ?? '',
+        props: {
+          from: id,
+          to: redirects.get(id)!,
+        },
+      }),
+    })
+  );
 
   console.info({
     method: 'POST',
