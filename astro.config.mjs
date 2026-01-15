7f364475ed694e98c4630cb72ef99ab99e813b42
tonyaellie <tonya@tokia.dev>
2026-01-15 13:38:58 +0000

    fix: errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 
 import cloudflare from '@astrojs/cloudflare';
 
@@ -17,7 +16,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     react(),
-    sitemap(),
   ],
   output: 'hybrid',
   adapter: cloudflare(),
